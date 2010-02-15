2a6bcc8b901e98222629106ca2f8b7acbb61fc58
Hibri Marzook <hibri@hibri.net>
2010-02-15 15:02:14 +0000

    Added solution to convert up to and above 200

---
+++ Dojo.FSharp/Roman.fs
@@ -0,0 +1,31 @@
+﻿// Learn more about F# at http://fsharp.net
+#light
+module Roman
+open System
+
+let  rec convert x =
+        if(x>=10 && x<40) then
+            "X" + convert(x-10)
+        elif(x>=40 && x<50) then
+            convert(50-x) + "L"
+        elif(x>=50 && x<90) then
+            "L" + convert(x-50)
+        elif(x >= 100) then
+            "C" + convert(x-100)
+        else
+            match Math.Abs((int)x) with
+            | 0 -> ""
+            | 1 -> "I"
+            | 2 -> "II"
+            | 3-> "III"
+            | 4 -> "IV"
+            | 5 -> "V"
+            | 6 -> "VI"
+            | 7 -> "VII"
+            | 8 -> "VIII"
+            | 9 -> "IX"
+            | x -> ""
+            
+    
+           
+
---
+++ Dojo.FSharp/RomanTests.fs
@@ -0,0 +1,72 @@
+﻿#light
+module RomanTests
+open Xunit
+open Roman
+
+   
+[<Fact>]
+let one_is_converted_to_I() =
+    Assert.Equal("I", Roman.convert 1)
+    
+[<Fact>]
+let two_is_II() = 
+    Assert.Equal("II", Roman.convert 2)
+    
+[<Fact>]
+let three_is_III() =
+    Assert.Equal("III", Roman.convert 3)
+    
+[<Fact>]
+let ten_is_X() =
+    Assert.Equal("X", Roman.convert 10)
+
+[<Fact>]
+let six_is_VI() =
+    Assert.Equal("VI", Roman.convert 6) 
+    
+[<Fact>]
+let eleven_is_XI() =
+    Assert.Equal("XI", Roman.convert 11)
+  
+[<Fact>]
+let five_is_V() =
+    Assert.Equal("V", Roman.convert 5)
+
+[<Fact>]
+let forty_is_XL() =
+    Assert.Equal("XL", Roman.convert 40)
+
+[<Fact>]
+let forty_nine_is_IL() =
+    Assert.Equal("IL", Roman.convert 49)
+    
+[<Fact>]
+let fifty_is_L() =
+    Assert.Equal("L", Roman.convert 50)
+    
+[<Fact>]
+let fifty_one_LI() =
+    Assert.Equal("LI", Roman.convert 51)
+   
+[<Fact>]
+let fifty_two_LII() =
+    Assert.Equal("LII", Roman.convert 52)
+[<Fact>]
+let sixty_one_LXI() =
+    Assert.Equal("LXI", Roman.convert 61)
+
+[<Fact>]
+let one_hundred_is_C() =
+    Assert.Equal("C", Roman.convert 100)
+
+[<Fact>]
+let one_hundred_and_one_is_CI() =
+    Assert.Equal("CI", Roman.convert 101)
+
+[<Fact>]
+let _200_is_CC() = 
+    Assert.Equal("CC", Roman.convert 200)
+
+[<Fact>]
+let _211_is_CCXI() =
+    Assert.Equal("CCXI", Roman.convert 211) 
