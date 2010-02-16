479c2f287a5be013a3c0268b367c272ca4e2d9cb
Hibri Marzook <hibri@hibri.net>
2010-02-16 14:42:57 +0000

    created RomanNUmber type, added is_left_of, is_right_of methods

--- Dojo.FSharp/Roman.fs
+++ Dojo.FSharp/Roman.fs
@@ -2,13 +2,26 @@
 #light
 module Roman
 open System
+type RomanNumber ={
+                    num:string;
+                    decimal_num:int; 
+                    left_bound:int; 
+                    right_bound:int; 
+                    }
+ 
+let is_left_of(x, n:RomanNumber) =
+    (x >= n.left_bound && x < n.decimal_num)
 
+let is_right_of(x, n:RomanNumber) =
+    (x >= n.decimal_num && x < n.right_bound)
+    
+   
 let  rec convert x =
         if(x>=10 && x<40) then
             "X" + convert(x-10)
-        elif(x>=40 && x<50) then
+        elif(is_left_of(x, { num="L"; decimal_num = 50; left_bound = 40; right_bound = 90;})) then
             convert(50-x) + "L"
-        elif(x>=50 && x<90) then
+        elif(is_right_of(x, { num="L"; decimal_num = 50; left_bound = 40; right_bound = 90;})) then
             "L" + convert(x-50)
         elif(x >= 100) then
             "C" + convert(x-100)
